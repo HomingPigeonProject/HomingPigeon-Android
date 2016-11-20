be402aa7d0790ffc3a4abf3350cf54e95eb4c0ee
Pierre Thévenet <thevenet.pierre@gmail.com>
2016-11-20 17:37:56 +0900

    php register to HomingPigeon

--- app/src/main/java/kaist/homingpigeon_android/MainActivity.java
+++ app/src/main/java/kaist/homingpigeon_android/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int READ_TIMEOUT=15000;
     private EditText etEmail;
     private EditText etPassword;
+    private EditText etUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         // Get Reference to variables
         etEmail = (EditText) findViewById(R.id.email);
         etPassword = (EditText) findViewById(R.id.password);
+        etUsername = (EditText) findViewById(R.id.username);
 
     }
 
@@ -46,11 +48,22 @@ public class MainActivity extends AppCompatActivity {
     public void checkLogin(View arg0) {
 
         // Get text from email and passord field
-        final String email = etEmail.getText().toString();
-        final String password = etPassword.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
 
         // Initialize  AsyncLogin() class with email and password
         new AsyncLogin().execute(email,password);
+
+    }
+
+    public void checkRegister(View arg0) {
+        // TODO : register
+
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String username = etUsername.getText().toString();
+
+        new AsyncRegister().execute(email, password, username);
 
     }
 
@@ -95,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // Append parameters to URL
                 Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("username", params[0])
+                        .appendQueryParameter("email", params[0])
                         .appendQueryParameter("password", params[1]);
                 String query = builder.build().getEncodedQuery();
 
@@ -180,4 +193,140 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    private class AsyncRegister extends AsyncTask<String, String, String>
+    {
+        ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+
+                // Enter URL address where your php file resides
+                url = new URL("http://vps332892.ovh.net/HomingPigeon/AndroidFiles/register.inc.php");
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return "exception";
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection)url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput method depict handling of both send and receive
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("email", params[0])
+                        .appendQueryParameter("password", params[1])
+                        .appendQueryParameter("username", params[2]);
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return "exception";
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return(result.toString());
+
+                }else{
+
+                    return("unsuccessful");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            } finally {
+                conn.disconnect();
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this method will be running on UI thread
+
+            pdLoading.dismiss();
+
+            if(result.equalsIgnoreCase("true"))
+            {
+                /* Here launching another activity when register successful. If you persist login state
+                use sharedPreferences of Android. and logout button to clear sharedPreferences.
+                 */
+                Toast.makeText(MainActivity.this, "Registration Success\n Please Log in", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+
+            } else if (result.equalsIgnoreCase("false")){
+
+                // If username and password does not match display a error message
+                Toast.makeText(MainActivity.this, "Invalid email or password", Toast.LENGTH_LONG).show();
+
+            } else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
+
+                Toast.makeText(MainActivity.this, "OOPs! Something went wrong. Connection Problem.", Toast.LENGTH_LONG).show();
+
+            } else if (result.equalsIgnoreCase("badEmail")) {
+                Toast.makeText(MainActivity.this, "Incorrect email", Toast.LENGTH_LONG).show();
+            } else if (result.equalsIgnoreCase("badPassword")) {
+                Toast.makeText(MainActivity.this, "Incorrect password", Toast.LENGTH_LONG).show();
+            } else if (result.equalsIgnoreCase("badUsername")) {
+                Toast.makeText(MainActivity.this, "Incorrect username", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
 }
