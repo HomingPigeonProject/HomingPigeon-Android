a7054366692c62ae8ab3b1dc531ee6d90961db89
Pierre Thévenet <thevenet.pierre@gmail.com>
2016-11-20 14:37:12 +0900

    commit before merging

--- app/src/main/java/kaist/homingpigeon_android/MainActivity.java
+++ app/src/main/java/kaist/homingpigeon_android/MainActivity.java
@@ -1,7 +1,10 @@
 package kaist.homingpigeon_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.open_activity_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 
     // test comment
